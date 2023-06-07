9dc8719f1e5cea9be85a1821d43dc78d49582b41
terezshashikyan <78693438+terezshashikyan@users.noreply.github.com>
2023-06-07 21:47:14 +0400

    some little changes

--- src/TodoInput.jsx
+++ src/TodoInput.jsx
@@ -4,9 +4,14 @@ import {useState} from "react";
 const TodoInput = ({addTodo}) => {
    
 const [text, setText] = useState('');
+
+const handleTextChange = (text) => {
+    setText(text)
+};
+
 return (
 <form onSubmit = {(e) => {e.preventDefault(), addTodo(text), setText("")}}>  
-<input type="text" value = {text} placeHolder = "Add Your Todos Here..." onChange = {(e) => {setText(e.target.value)}}/>
+<input type="text" value = {text} placeHolder = "Add Your Todos Here..." onChange = {(e) => {handleTextChange(e.target.value)}}/>
 <button  className = "AddButton">Add</button>
 </form>
 )
